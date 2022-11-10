a64883147aa0fad5b32de4b8f133cf26d7c6e3ee
dongs365 <dongs365@mooom.top>
2022-11-10 23:57:58 +0800

    2022年 11月 10日 星期四 23:57:58 CST

---
+++ precache-manifest.fad669283f3e52523d95b7f7007ce23a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da8cd71c9d2a94a6247e9986967381c6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9132d60c4d5a79f52156",
+    "url": "/static/css/main.e2e601f0.chunk.css"
+  },
+  {
+    "revision": "1b6ba93fa5daf7299ae5",
+    "url": "/static/js/2.04a3d951.chunk.js"
+  },
+  {
+    "revision": "9132d60c4d5a79f52156",
+    "url": "/static/js/main.bdcc4006.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
